824aa3c27b2fefc896da00081b7bdc7ea53a0ad8
eunseo <hjing719@gmail.com>
2024-03-14 22:11:10 +0900

    컬러에셋 확장

---
+++ MyShoppingList/Swift/UIColor+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Extension.swift
+//  MyShoppingList
+//
+//  Created by 이은서 on 3/14/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let sky = UIColor(named: "sky")
+    static let gold = UIColor(named: "gold")
+}
